568aa62fdb614c2ce53d6177fa86e195b7de24f4
Manuel Naranjo <manuelnaranjo@outlook.com>
2019-06-28 14:31:29 -0500

    First test version

---
+++ D365FOrestservices/D365FOrestservices/ODataTest.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.OData.Client;
+using D365FOrestservices.Microsoft.Dynamics.DataEntities;
+using System.Collections;
+
+namespace D365FOrestservices
+{
+    class ODataUserContract
+    {
+        public string userName;
+        public string password;
+        public string domain;
+    }
+    class ODataApplicationContract
+    {
+        public string applicationId;
+        public string resource;
+        public string result;
+    }
+    public class ODataRequestContract
+    {
+        public string company;
+    }
+
+    class ODataTest
+    {
+        public const string OAuthHeader = "Authorization";
+        public ODataUserContract userContract;
+        public ODataApplicationContract appContract;
+        public ODataRequestContract request;
+        string authenticationHeader;
+        public string response;
+
+        private AuthenticationResult GetAuthorization()
+        {
+            UriBuilder uri = new UriBuilder("https://login.windows.net/" + userContract.domain);
+            AuthenticationContext authenticationContext
+            = new AuthenticationContext(uri.ToString());
+            UserPasswordCredential credential = new
+            UserPasswordCredential(
+            userContract.userName, userContract.password);
+            Task<AuthenticationResult> task = authenticationContext.AcquireTokenAsync(
+            appContract.resource,
+            appContract.applicationId, credential);
+            task.Wait();
+            AuthenticationResult
+            authenticationResult = task.Result;
+            return authenticationResult;
+        }
+
+        public Boolean Authenticate()
+        {
+            AuthenticationResult authenticationResult;
+            try
+            {
+                authenticationResult = GetAuthorization();
+                //this gets the authorization token, this
+                // must be set on the Http header for all requests
+                authenticationHeader =
+                authenticationResult.CreateAuthorizationHeader();
+            }
+            catch (Exception e)
+            {
+                response = "Authentication failed: " + e.Message;
+                return false;
+            }
+            response = "OK";
+            return true;
+        }
+
+        private Resources MakeResources()
+        {
+            string entityRootPath = appContract.resource + "/data";
+            Uri oDataUri = new Uri(entityRootPath,
+            UriKind.Absolute);
+            var resources = new Resources(oDataUri);
+            resources.SendingRequest2 += new
+            EventHandler<SendingRequest2EventArgs>(
+            delegate (object sender,
+            SendingRequest2EventArgs e)
+            {
+                // This event handler is needed to set
+                // the authentication code we got when
+                // we logged on.
+                e.RequestMessage.SetHeader(OAuthHeader,
+                authenticationHeader);
+            });
+            return resources;
+        }
+
+        public System.Collections.ArrayList GetSalesQuotationNameList()
+        {
+            ArrayList array = new ArrayList();
+            var resources = this.MakeResources();
+
+            resources.SalesQuotationHeadersV2.AddQueryOption("DataAreaId", request.company);
+
+            foreach (var obj in resources.SalesQuotationHeadersV2)
+            {
+                array.Add(obj.SalesQuotationName);
+            }
+
+            return array;
+        }
+
+        /*public Boolean UpdateVehicleNames()
+        {
+            var resources = this.MakeResources();
+            resources.SalesQuotationHeadersV2.AddQueryOption("DataAreaId", request.company);
+            foreach (var vehicle in resources.SalesQuotationHeadersV2)
+            {
+                vehicle.Description = vehicle.VehicleId
+                + " : OData did it";
+                resources.UpdateObject(vehicle);
+            }
+            try
+            {
+                resources.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                response = e.InnerException.Message;
+                return false;
+            }
+            return true;
+        }*/
+
+        /*public Boolean CreateNewVehicle(
+        ConWHSVehicleTable _newVehicle)
+        {
+            var resources = this.MakeResources();
+            _newVehicle.DataAreaId = request.company;
+            resources.AddToConWHSVehicleTables(_newVehicle);
+            try
+            {
+                resources.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                response = e.InnerException.Message;
+                return false;
+            }
+            return true;
+        }*/
+    }
+}
--- D365FOrestservices/D365FOrestservices/Program.cs
+++ D365FOrestservices/D365FOrestservices/Program.cs
@@ -1,17 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+using D365FOrestservices;
 
-namespace D365FOrestservices
+namespace ConsoleApplication
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            ODataApplicationContract appContract;
+            appContract = new ODataApplicationContract();
+            appContract.resource = "https://bioresearch-devdevaos.sandbox.ax.dynamics.com/";
+            appContract.applicationId = "6ee49733-a2ed-47a2-8dd3-be070d0a38d6";
+            ODataUserContract userContract = new
+            ODataUserContract();
+            Console.WriteLine("Use the O365 account that you use to log in to Dynamics 365 for Operations");
+            Console.Write("O365 Username: ");
+            userContract.userName = Console.ReadLine();
+            Console.Write("O365 Password: ");
+            userContract.password = Console.ReadLine();
+            Console.WriteLine("This is your tenant, such as yourdomain.com or <yourtenant>.onmicrosoft.com");
+            Console.Write("O365 Domain: ");
+            userContract.domain = Console.ReadLine();
+            ODataTest test = new ODataTest();
+            test.userContract = userContract;
+            test.appContract = appContract;
+            if (!test.Authenticate())
+            {
+                Console.WriteLine(test.response);
+            }
+            test.request = new ODataRequestContract();
+            test.request.company = "USMF";
+            System.Collections.ArrayList vehicleNames = test.GetSalesQuotationNameList();
+            foreach (var vehicleName in vehicleNames)
+            {
+                Console.WriteLine(vehicleName);
+            }
+            Console.WriteLine("Changing vehicle descriptions");
+            test.GetSalesQuotationNameList();
+            /*SalesQuota vehicle = new ConWHSVehicleTable();
+            Console.WriteLine("Create a new Vehicle");
+            Console.Write("Vehicle Id: ");
+            vehicle.VehicleId = Console.ReadLine();
+            Console.Write("Vehicle group: ");
+            vehicle.VehicleGroupId = Console.ReadLine();
+            Console.Write("Description: ");
+            vehicle.Description = Console.ReadLine();
+            test.CreateNewVehicle(vehicle);
+            Console.WriteLine("Press enter to continue.");*/
+            Console.ReadLine();
         }
     }
 }
