93912cd8502157708bc73deac372e13e580ccaeb
Manuel Naranjo <manuelnaranjo@outlook.com>
2019-07-01 23:36:03 -0500

    Test succeed 2

--- D365FOrestservices/D365FOrestservices/Program.cs
+++ D365FOrestservices/D365FOrestservices/Program.cs
@@ -12,17 +12,18 @@ namespace ConsoleApplication
             string user = "";
             string pass = "";
             string authorityUrl = "";
+            string clientId = "";
             string resource = "";
             string requestUri = "";
 
             var credentials = new UserPasswordCredential(user, pass);
             var context = new AuthenticationContext(authorityUrl);
-            var authResult = context.AcquireTokenAsync(resource, "6ee49733-a2ed-47a2-8dd3-be070d0a38d6", credentials).Result;
+            var authResult = context.AcquireTokenAsync(resource, clientId, credentials).Result;
 
             using (HttpClient httpClient1 = new HttpClient())
             {
                 httpClient1.BaseAddress = new Uri(resource);
-                httpClient1.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", authResult.AccessToken);
+                httpClient1.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authResult.AccessToken);
                 HttpResponseMessage response = httpClient1.GetAsync(requestUri).Result;
                 if (response.IsSuccessStatusCode)
                 {
