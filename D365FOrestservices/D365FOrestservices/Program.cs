8e2acaa4b3fe6ce931255ecdedb876c4d8a5bf82
Manuel Naranjo <manuelnaranjo@outlook.com>
2019-07-10 12:49:18 -0500

    Added a new method to connect using client secret

--- D365FOrestservices/D365FOrestservices/Program.cs
+++ D365FOrestservices/D365FOrestservices/Program.cs
@@ -2,22 +2,24 @@
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace ConsoleApplication
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            string user = "";
-            string pass = "";
-            string authorityUrl = "";
-            string clientId = "";
-            string resource = "";
-            string requestUri = "";
+        string user = "";
+        string pass = "";
+        string authority = "";
+        string clientId = "";
+        string resource = "";
+        string requestUri = "";
+        string clientSecret = "";
 
+        public void test1()
+        {
             var credentials = new UserPasswordCredential(user, pass);
-            var context = new AuthenticationContext(authorityUrl);
+            var context = new AuthenticationContext(authority);
             var authResult = context.AcquireTokenAsync(resource, clientId, credentials).Result;
 
             using (HttpClient httpClient1 = new HttpClient())
@@ -30,10 +32,42 @@ namespace ConsoleApplication
                     System.Console.WriteLine("Success");
                 }
                 string message = response.Content.ReadAsStringAsync().Result;
-                System.Console.WriteLine("URL responese : " + message);
+                Console.WriteLine("URL responese : " + message);
             }
+        }
+
+        public async void test2()
+        {
+            HttpClient client = new HttpClient();
+
+            // get the token
+            
+            var clientCredential = new ClientCredential(clientId, clientSecret);
+            AuthenticationContext context = new AuthenticationContext(authority, false);
+            AuthenticationResult authenticationResult = await context.AcquireTokenAsync(resource, clientCredential); 
+
+            // set the auth header with the aquired Bearer token
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authenticationResult.AccessToken);
+
+            // make the call to the resource requiring auth!
+            var resp = await client.GetAsync(resource + "//" + requestUri);
+
+            // do something with the response
+            string message = resp.Content.ReadAsStringAsync().Result;
+            Console.WriteLine("URL responese : " + message);
+        }
+
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            //obj.test1();
+            obj.test2();
 
             Console.ReadLine();
+        }
+
+        public Program()
+        {
         }
     }
 }
